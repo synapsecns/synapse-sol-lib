e90c63b9e082a0649b72394cab7c7d60679f290b
Bleaker <maozdror@protonmail.com>
2022-02-03 14:42:11 -0500

    Add extended IERC20 interface

---
+++ contracts/solc8/erc20/IERC20.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import {IERC20 as _IERC20} from "@openzeppelin/contracts-solc8/token/ERC20/IERC20.sol";
+
+/**
+ * @dev Interface of the ERC20 standard as defined in the EIP.
+ */
+interface IERC20 is _IERC20 {
+    function nonces(address) external view returns (uint256); // Only tokens that support permit
+
+    function permit(
+        address,
+        address,
+        uint256,
+        uint256,
+        uint8,
+        bytes32,
+        bytes32
+    ) external; // Only tokens that support permit
+
+    function mint(address to, uint256 amount) external; // only tokens that support minting
+}
